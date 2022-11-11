1887fccad17eb1dbf07448fec1d314089e7f89a6
AlyGallo <118003074+AlyGallo@users.noreply.github.com>
2022-11-11 12:47:15 -0300

    POO
    sistema de celular poo

---
+++ POO.dio/trilha-net-poo-desafio-main/Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Testando Nokia");
+Smartphone nokia = new Nokia("0116", "Tijolo", "Gus123", 1234 );
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Testando Iphone");
+Smartphone iphone = new Iphone("1914", "14 Pro", "Cris123", 4321);
+iphone.Ligar();
+iphone.InstalarAplicativo("Snapchat");
